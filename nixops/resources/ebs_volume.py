20f47b6915d3b519c729a9cc70379514e2e74d0e
Tewfik Ghariani <tewfik.ghariani@infor.com>
2020-01-13 17:45:29 +0100

    Displaying EBS size in nixops-info

--- nixops/resources/ebs_volume.py
+++ nixops/resources/ebs_volume.py
@@ -56,7 +56,7 @@ class EBSVolumeState(nixops.resources.ResourceState, nixops.resources.ec2_common
 
     def show_type(self):
         s = super(EBSVolumeState, self).show_type()
-        if self._exists(): s = "{0} [{1}]".format(s, self.zone)
+        if self._exists(): s = "{0} [{1}; {2} GiB]".format(s, self.zone, self.size)
         return s
 
 
